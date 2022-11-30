ee6fd4d04648d5d21329c82c4243227d21a1507b
Bao <752354226@qq.com>
2022-11-30 15:54:20 +0000

    Solution for leetcode exercise 42 with JAVA.

---
+++ Exercise42/JAVA/untitled/src/package1/Solution2.java
@@ -0,0 +1,33 @@
+package package1;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution2 {
+    boolean solution;
+
+    public Solution2(){
+        this.solution = true;
+    }
+
+
+        public int trap(int[] height) {
+            int ans = 0;
+            Deque<Integer> stack = new LinkedList<Integer>();
+            int n = height.length;
+            for (int i = 0; i < n; ++i) {
+                while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                    int top = stack.pop();
+                    if (stack.isEmpty()) {
+                        break;
+                    }
+                    int left = stack.peek();
+                    int currWidth = i - left - 1;
+                    int currHeight = Math.min(height[left], height[i]) - height[top];
+                    ans += currWidth * currHeight;
+                }
+                stack.push(i);
+            }
+            return ans;
+        }
+}
---
+++ Exercise42/JAVA/untitled/src/package1/Solution3.java
@@ -0,0 +1,27 @@
+package package1;
+
+public class Solution3 {
+    boolean solution;
+
+    public Solution3(){
+        this.solution = true;
+    }
+
+    public int trap(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+}
